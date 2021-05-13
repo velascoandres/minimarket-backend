b8237adaa8148e98a58af1fbfeb2cc132e2e87d2
andres velasco <velasco.andrs@gmail.com>
2021-05-13 16:19:02 -0500

    add gcs example

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,23 +1,37 @@
-import { CrudApi, CrudController, CrudOptions } from '@nest-excalibur/common-api/lib';
-import { Controller } from '@nestjs/common';
+import { CrudController, CrudOptions } from '@nest-excalibur/common-api/lib';
+import {
+  GoogleCloudStorageService,
+  UploadedFileMetadata,
+} from '@nest-excalibur/google-cloud-storage';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ProductCreateDTO, ProductUpdateDto } from './dto/product.dto';
 import { ProductService } from './product.service';
 
 const options: CrudOptions = {
-    dtoConfig: {
-        createDtoType: ProductCreateDTO,
-        updateDtoType: ProductUpdateDto,
-    },
-}
-
+  dtoConfig: {
+    createDtoType: ProductCreateDTO,
+    updateDtoType: ProductUpdateDto,
+  },
+};
 
 @Controller('product')
 export class ProductController extends CrudController(options) {
+  constructor(
+    private readonly productService: ProductService,
+    private readonly googleCloudStorageService: GoogleCloudStorageService,
+  ) {
+    super(productService);
+  }
 
-    constructor(
-        private readonly productService: ProductService,
-    ){
-        super(productService);
-    }
-
+  @Post('upload-picture')
+  @UseInterceptors(FileInterceptor('picture'))
+  uploadProductLogo(@UploadedFile() pictureFile: UploadedFileMetadata) {
+    return this.googleCloudStorageService.upload(pictureFile);
+  }
 }
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -6,21 +6,20 @@ import { ProductEntity } from './product.entity';
 import { DataBaseModule } from '@nest-excalibur/data-base';
 import { ProductCreateDTO } from './dto/product.dto';
 
+import { GoogleCloudStorageModule } from '@nest-excalibur/google-cloud-storage/lib';
+
 @Module({
   imports: [
-    TypeOrmModule.forFeature(
-      [ProductEntity]
-    ),
-    DataBaseModule.forBulkData(
-      {
-        creationOrder: 2,
-        pathDev: '/src/product/mocks/products.json',
-        entity: ProductEntity,
-        dtoClassValidation: ProductCreateDTO,
-      }
-    ),
+    TypeOrmModule.forFeature([ProductEntity]),
+    DataBaseModule.forBulkData({
+      creationOrder: 2,
+      pathDev: '/src/product/mocks/products.json',
+      entity: ProductEntity,
+      dtoClassValidation: ProductCreateDTO,
+    }),
+    GoogleCloudStorageModule.register({ bucketDefaultName: 'pimba_test_gcs' }),
   ],
   providers: [ProductService],
-  controllers: [ProductController]
+  controllers: [ProductController],
 })
 export class ProductModule {}
